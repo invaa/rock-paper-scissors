03610b930b7eb02b2176275481dc534dfbce7e45
Oleksandr Zamkovyi <alex@zamkovyi.name>
2019-06-06 00:27:05 +0300

    more tests

---
+++ src/test/java/com/games/rps/api/service/MarkovDecisionEngineTest.java
@@ -0,0 +1,84 @@
+package com.games.rps.api.service;
+
+import com.games.rps.api.model.Move;
+import com.games.rps.api.repo.ProbabilitiesRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static com.games.rps.api.model.Move.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MarkovDecisionEngineTest {
+
+    @Mock
+    private ProbabilitiesRepository probabilitiesRepositoryMock;
+
+    @InjectMocks
+    private MarkovDecisionEngine markovDecisionEngine;
+
+    @Test
+    public void shouldReturnRandomMoveWhenNoStatsAndSaveStats() {
+        // given
+        String playerId = "player1";
+        when(probabilitiesRepositoryMock.hasNoGamesPlayed(playerId)).thenReturn(true);
+
+        // when
+        Move predictedMove = markovDecisionEngine.act(playerId, ROCK);
+
+        // then
+        assertNotNull(predictedMove);
+        verify(probabilitiesRepositoryMock).updateChain(playerId, ROCK);
+    }
+
+    @Test
+    public void shouldReturnPredictedMoveWhenHasStatsForPaper() {
+        // given
+        String playerId = "player1";
+        when(probabilitiesRepositoryMock.hasNoGamesPlayed(playerId)).thenReturn(false);
+        when(probabilitiesRepositoryMock.getChain(playerId)).thenReturn(new Long[]{100L,0L,0L});
+
+        // when
+        Move predictedMove = markovDecisionEngine.act(playerId, SCISSORS);
+
+        // then
+        assertEquals(predictedMove, PAPER);
+        verify(probabilitiesRepositoryMock).updateChain(playerId, SCISSORS);
+    }
+
+    @Test
+    public void shouldReturnPredictedMoveWhenHasStatsForScissors() {
+        // given
+        String playerId = "player1";
+        when(probabilitiesRepositoryMock.hasNoGamesPlayed(playerId)).thenReturn(false);
+        when(probabilitiesRepositoryMock.getChain(playerId)).thenReturn(new Long[]{0L,100L,0L});
+
+        // when
+        Move predictedMove = markovDecisionEngine.act(playerId, PAPER);
+
+        // then
+        assertEquals(predictedMove, SCISSORS);
+        verify(probabilitiesRepositoryMock).updateChain(playerId, PAPER);
+    }
+
+    @Test
+    public void shouldReturnPredictedMoveWhenHasStatsForRock() {
+        // given
+        String playerId = "player1";
+        when(probabilitiesRepositoryMock.hasNoGamesPlayed(playerId)).thenReturn(false);
+        when(probabilitiesRepositoryMock.getChain(playerId)).thenReturn(new Long[]{100L,0L,0L});
+
+        // when
+        Move predictedMove = markovDecisionEngine.act(playerId, ROCK);
+
+        // then
+        assertEquals(predictedMove, PAPER);
+        verify(probabilitiesRepositoryMock).updateChain(playerId, ROCK);
+    }
+}
---
+++ src/test/java/com/games/rps/api/service/ResultEvaluatorTest.java
@@ -0,0 +1,69 @@
+package com.games.rps.api.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static com.games.rps.api.model.Move.*;
+import static com.games.rps.api.model.Result.*;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ResultEvaluatorTest {
+
+    private final ResultEvaluator resultEvaluator = new ResultEvaluator();
+
+    @Test
+    public void shouldReturnDrawWhenRockRock() {
+        // given when then
+        assertTrue(DRAW.equals(resultEvaluator.evaluateMoves(ROCK, ROCK)));
+    }
+
+    @Test
+    public void shouldReturnLoseWhenRockPaper() {
+        // given when then
+        assertTrue(LOSE.equals(resultEvaluator.evaluateMoves(ROCK, PAPER)));
+    }
+
+    @Test
+    public void shouldReturnWinWhenRockScissors() {
+        // given when then
+        assertTrue(WIN.equals(resultEvaluator.evaluateMoves(ROCK, SCISSORS)));
+    }
+
+    @Test
+    public void shouldReturnWinWhenPaperRock() {
+        // given when then
+        assertTrue(WIN.equals(resultEvaluator.evaluateMoves(PAPER, ROCK)));
+    }
+
+    @Test
+    public void shouldReturnLoseWhenPaperPaper() {
+        // given when then
+        assertTrue(DRAW.equals(resultEvaluator.evaluateMoves(PAPER, PAPER)));
+    }
+
+    @Test
+    public void shouldReturnWinWhenPaperScissors() {
+        // given when then
+        assertTrue(LOSE.equals(resultEvaluator.evaluateMoves(PAPER, SCISSORS)));
+    }
+
+    @Test
+    public void shouldReturnWinWhenScissorsRock() {
+        // given when then
+        assertTrue(LOSE.equals(resultEvaluator.evaluateMoves(SCISSORS, ROCK)));
+    }
+
+    @Test
+    public void shouldReturnLoseWhenScissorsPaper() {
+        // given when then
+        assertTrue(WIN.equals(resultEvaluator.evaluateMoves(SCISSORS, PAPER)));
+    }
+
+    @Test
+    public void shouldReturnWinWhenScissorsScissors() {
+        // given when then
+        assertTrue(DRAW.equals(resultEvaluator.evaluateMoves(SCISSORS, SCISSORS)));
+    }
+}
