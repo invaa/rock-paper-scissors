db425ba058b4d5b4f72e4f388c870caedf0e8687
Oleksandr Zamkovyi <alex@zamkovyi.name>
2019-06-06 11:38:18 +0300

    reverted disabling ctor

--- src/main/java/com/games/rps/api/RpsApplication.java
+++ src/main/java/com/games/rps/api/RpsApplication.java
@@ -1,7 +1,5 @@
 package com.games.rps.api;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -10,7 +8,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableAsync
 @EnableSwagger2
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RpsApplication {
     public static void main(String[] args) {
         SpringApplication.run(RpsApplication.class, args);
